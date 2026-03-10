36dbc7e87b1b6cf13eb977a84a46fba161ae19c3
JUANCHOS123 <daianalizzano92@gmail.com>
2026-03-10 13:56:26 -0300

    Eliminar test-bot.js

--- api/test-bot.js
+++
@@ -1,7 +0,0 @@
-fetch('https://generador-imagen.vercel.app/api/discord-fix', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ type: 1 })
-})
-.then(r => r.json())
-.then(console.log)
